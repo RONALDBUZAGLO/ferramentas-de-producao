fdbde4cbdd99de9cd1c63bba8757130c4145d688
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-04 14:54:43 -0400

    Revert "Flash-connect, Botões do cadastro"
    This reverts commit 080c1667ee93329ff56728d73d0957693528c9da.

--- rotas/Controller.js
+++ rotas/Controller.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const qr = require("qrcode");
 const Dados = require('../database/Dados');
-const flash = require('connect-flash');
-const session = require('express-session');
 
 router.post("/gerador",(req,res)=>{
     const url = req.body.url;
@@ -18,79 +16,69 @@ router.post("/gerador",(req,res)=>{
     })
 });
 
+
+
 router.get("/cadastro",(req,res)=>{
     res.render('pages/cadastro');
 });
 
-//
+
+
 router.post("/gerarCode",(req,res)=>{
 
-    const salvar = req.body.salvar;
-    const etiqueta = req.body.etiqueta;
+    const codigoDoPrograma = req.body.codigoDoPrograma;
+    const lado = req.body.lado;
+    const revisaoSerie = req.body.revisaoSerie;
+    const mf = req.body.mf;
+    const stencilId = req.body.stencilId;
+    const descricao = req.body.descricao;
+    const modificacao = req.body.modificacao;
 
-    if (!!salvar || !!etiqueta) {
+    Dados.create({
+        codigoDoPrograma,
+        lado,
+        revisaoSerie,
+        mf,
+        stencilId,
+        descricao,
+        modificacao,
+    }).then(()=>{
         
-        const codigoDoPrograma = req.body.codigoDoPrograma;
-        const lado = req.body.lado;
-        const revisaoSerie = req.body.revisaoSerie;
-        const mf = req.body.mf;
-        const stencilId = req.body.stencilId;
-        const descricao = req.body.descricao;
-        const modificacao = req.body.modificacao;
+        const concatenado = 
+            codigoDoPrograma + '/' +
+            lado + '/' +
+            revisaoSerie + '/' +
+            mf + '/' +
+            stencilId + '/' +
+            descricao + '/' +
+            modificacao;
+        
+        if (concatenado.length <= 6){
+            res.send("Empty data!");
+        }
 
-        Dados.create({
-            codigoDoPrograma,
-            lado,
-            revisaoSerie,
-            mf,
-            stencilId,
-            descricao,
-            modificacao,
-        }).then(()=>{
-            
-            const concatenado = 
-                codigoDoPrograma + '/' +
-                lado + '/' +
-                revisaoSerie + '/' +
-                mf + '/' +
-                stencilId + '/' +
-                descricao + '/' +
-                modificacao;
-            
-            if (concatenado.length <= 6){
-                res.send("Empty data!");
+        qr.toDataURL(concatenado,{ errorCorrectionLevel: 'H', version: 7 },(err,src)=>{
+        
+            if (err) {
+                res.send("Aconteceu um erro: " + err + "\nProcure suporte de programação!");
             }
-
-            qr.toDataURL(concatenado,{ errorCorrectionLevel: 'H', version: 7 },(err,src)=>{
             
-                if (err) {
-                    res.send("Aconteceu um erro: " + err + "\nProcure suporte de programação!");
-                }
-                
-                req.flash('info','flash is back !');
-                
-
-                res.render("pages/saida",{
-                    message: req.flash('info'),
-                    src,
-                    codigoDoPrograma,
-                    lado,
-                    revisaoSerie,
-                    stencilId,
-                    mf,
-                });
-
+            res.render("pages/saida",{
+                src,
+                codigoDoPrograma,
+                lado,
+                revisaoSerie,
+                stencilId,
+                mf,
             });
 
-        }).catch((err)=>{
-            console.log("erro ao salvar dados: "+err);
-            res.redirect('/cadastro');
         });
 
-        }else{
-            res.redirect('/cadastro');
-        }
-        
+    }).catch((err)=>{
+        console.log("erro ao salvar dados: "+err);
+        res.redirect('/cadastro');
     });
+
+});
 
 module.exports = router;
