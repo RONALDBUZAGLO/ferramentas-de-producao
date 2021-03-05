0756720796cabededcd0e9ec038082cc96fc79eb
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-05 16:18:23 -0400

    correção no PORT

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ app.get("/",(req,res)=>{
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
-    console.log("servidor rodando na porta "+ port);
+    console.log("servidor rodando na porta "+ PORT);
 });
