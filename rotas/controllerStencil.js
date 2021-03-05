f459923f6ca85606f776b85b6d87f3e05f93c969
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-05 12:03:06 -0400

    Configuração e Exibição das informações salvas em banco de dados em formato de lista

--- rotas/controllerStencil.js
+++ rotas/controllerStencil.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Dados = require('../database/Dados');
 
 
 //ROTAS STENCIL
 router.get("/stencils-cadastrados",(req,res)=>{
-    req.flash('message','Stencil cadastrado com sucesso!');
-    res.render('pages/menu/stencil/stencils-cadastrados',{message: req.flash('message')});
+    Dados.findAll({raw: true}).then((stencils)=>{
+        console.log(stencils);
+        req.flash('message','Stencil cadastrado com sucesso!');
+        res.render('pages/menu/stencil/stencils-cadastrados',{
+            message: req.flash('message'),
+            stencils: stencils,
+        });
+    })
+
 });
 
 
